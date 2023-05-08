f371afb8c8caeed4dd77d36194b2697b6aa356d6
Kodeberjalan <59494446+Kodeberjalan@users.noreply.github.com>
2023-05-08 12:29:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sasl02/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.sasl02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv1= findViewById(R.id.tvHasil);
+        EditText et1=findViewById(R.id.etangka1);
+        EditText et2=findViewById(R.id.etangka2);
+        Button bt1=findViewById(R.id.jumlah);
+        Button bt2=findViewById(R.id.kurang);
+        Button bt3=findViewById(R.id.kali);
+        Button bt4=findViewById(R.id.bagi);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String jumlah=" ";
+                if(view.getId()==bt1.getId()){
+                    int x1= Integer.parseInt(et1.getText().toString());
+                    int x2= Integer.parseInt(et2.getText().toString());
+                    int hasil= x1 + x2;
+                    jumlah=String.valueOf(hasil);
+                    tv1.setText(jumlah);
+                }
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Jumlah= " ";
+                if(view.getId()==bt2.getId()){
+                    int x1=Integer.parseInt(et1.getText().toString());
+                    int x2=Integer.parseInt(et2.getText().toString());
+                    int hasil=x1-x2;
+                    Jumlah=String.valueOf(hasil);
+                    tv1.setText(Jumlah);
+                }
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Jumlah= " ";
+                if(view.getId()==bt3.getId()){
+                    int x1=Integer.parseInt(et1.getText().toString());
+                    int x2=Integer.parseInt(et2.getText().toString());
+                    int hasil=x1*x2;
+                    Jumlah=String.valueOf(hasil);
+                    tv1.setText(Jumlah);
+                }
+            }
+        });
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Jumlah= " ";
+                if(view.getId()==bt4.getId()){
+                    int x1=Integer.parseInt(et1.getText().toString());
+                    int x2=Integer.parseInt(et2.getText().toString());
+                    float hasil=x1/x2;
+                    Jumlah=String.valueOf(hasil);
+                    tv1.setText(Jumlah);
+                }
+            }
+        });
+    }
+}
